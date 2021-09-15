89b51416136ac982f382d6362a9b22b389d6e243
Crowdbotics <team@crowdbotics.com>
2021-09-15 15:17:48 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "bitter_unit_30576Identifier",
+  url: "https://bitter_unit_30576Identifier.botics.co",
+  api: "https://bitter_unit_30576Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
